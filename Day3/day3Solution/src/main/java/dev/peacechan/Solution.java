63a53afe36b7d6f83d8dcc150f26a2b9fde62638
AyeChanHtunNaing <ayechanhtunnaing@icloud.com>
2024-07-05 00:43:12 +0630

    added better solution for day3

--- Day3/day3Solution/src/main/java/dev/peacechan/Solution.java
+++ Day3/day3Solution/src/main/java/dev/peacechan/Solution.java
@@ -16,15 +16,15 @@ public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[] nums1 = {5, 3, 2, 4};
-        System.out.println("Input : " + Arrays.toString(nums1));
-        System.out.println("Output : " + solution.minDifference(nums1));
+        System.out.println("Input :  " + Arrays.toString(nums1));
+        System.out.println("Output :  " + solution.minDifference(nums1));
 
         int[] nums2 = {1, 5, 0, 10, 14};
-        System.out.println("Input : " + Arrays.toString(nums2));
-        System.out.println("Output : " + solution.minDifference(nums2));
+        System.out.println("Input :  " + Arrays.toString(nums2));
+        System.out.println("Output :  " + solution.minDifference(nums2));
 
         int[] nums3 = {3, 100, 20};
-        System.out.println("Input : " + Arrays.toString(nums3));
-        System.out.println("Output : " + solution.minDifference(nums3));
+        System.out.println("Input :  " + Arrays.toString(nums3));
+        System.out.println("Output :  " + solution.minDifference(nums3));
     }
 }
